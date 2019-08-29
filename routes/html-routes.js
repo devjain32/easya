c4036016d0012500b83b565f249fcba7e3a16681
Dev Jain <devrjain32200@gmail.com>
2019-08-29 11:53:05 -0500

    inserted scheduling and reservation

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -12,6 +12,12 @@ module.exports = function (app) {
   app.get("/dashboard", isAuthenticated, function (req, res) {
     res.sendFile(path.join(__dirname, "../public/dashboard.html"));
   });
+  app.get("/reserve", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/reserve.html"));
+  });
+  app.get("/buy", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/buy.html"));
+  });
   app.get("*", isAuthenticated, function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
